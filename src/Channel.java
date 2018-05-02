b7cfd26820c391ff27dcc43340c130a99ee25bac
ableigdev <igor.yarovoy.98@ukr.net>
2018-05-02 12:25:29 +0300

    Added request and full time request counters

--- src/Channel.java
+++ src/Channel.java
@@ -5,13 +5,17 @@ public class Channel
             m_State = m_State.Free;
             m_Request = 0;
             m_Time = 0; 
+            m_CounterRequest = 0;
+            m_FullTimeRequest = 0;
 	}
 	
 	public void addRequest(int value, int time)
 	{
             m_Request = value;
             setTime(time);
-            work();        
+            work();  
+            ++m_CounterRequest;
+            m_FullTimeRequest += time;
 	}
         
         public void setTime(int value)
@@ -39,6 +43,16 @@ public class Channel
             return m_State;
 	}
         
+        public int getCounterRequest()
+        {
+            return m_CounterRequest;
+        }
+        
+        public int getFullTimeRequest()
+        {
+            return m_FullTimeRequest;
+        }
+        
         private void work()
         {
             while (m_Time != 0)
@@ -50,4 +64,6 @@ public class Channel
 	private ChannelState m_State;
         private int m_Request;
         private int m_Time;
+        private int m_CounterRequest;
+        private int m_FullTimeRequest;
 }
