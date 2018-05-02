89b5f8f145d5dfd3b8be01acfaec1ab06e88061b
ableigdev <igor.yarovoy.98@ukr.net>
2018-05-02 12:26:13 +0300

    Added request counter

--- src/StorageDevice.java
+++ src/StorageDevice.java
@@ -5,11 +5,13 @@ public class StorageDevice
     StorageDevice()
     {
         m_State = new LinkedList<>();
+        m_CounterRequest = 0;
     }
 
     public void addRequest(int value)
     {
         m_State.offer(value);
+        m_CounterRequest = m_State.size() > m_CounterRequest ? m_State.size() : m_CounterRequest;
     }
 
     public int removeRequest()
@@ -22,5 +24,11 @@ public class StorageDevice
         return m_State.size();
     }
     
+    public int counterRequest()
+    {
+        return m_CounterRequest;
+    }
+    
     private Queue<Integer> m_State;
+    private int m_CounterRequest;
 }
