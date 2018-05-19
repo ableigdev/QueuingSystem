f96e25bca0d5b6f4ebe5a0d4607a0397ce3ed557
ableigdev <igor.yarovoy.98@ukr.net>
2018-05-19 18:17:21 +0300

    Added work time

--- src/Channel.java
+++ src/Channel.java
@@ -5,6 +5,7 @@ public class Channel
             m_State = m_State.Free;
             m_Request = null;
             m_Time = 0; 
+            m_WorkTime = 0;
             m_CounterRequest = 0;
             m_FullTimeRequest = 0;
 	}
@@ -21,11 +22,17 @@ public class Channel
         public void setTime(int value)
         {
             m_Time = value > 0 ? value : 0;
+            m_WorkTime = m_Time;
         }
         
         public int getTime()
         {
             return m_Time;
+        }
+        
+        public int getWorkTime()
+        {
+            return m_WorkTime;
         }
 	
 	public Request removeRequest()
@@ -64,6 +71,7 @@ public class Channel
 	private ChannelState m_State;
         private Request m_Request;
         private int m_Time;
+        private int m_WorkTime;
         private int m_CounterRequest;
         private int m_FullTimeRequest;
 }
